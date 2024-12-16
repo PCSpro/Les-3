91bb94e5629f9d0e7a982884f8158311890576f5
PCSpro <drago110103@gmail.com>
2024-12-16 15:23:45 +0500

    Add files via upload

---
+++ Распаковка позиционных параметров.py
@@ -0,0 +1,15 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a, b, c)
+
+print_params(b = 25)
+print_params(c = [1,2,3])
+
+values_list = ['VIN', True, 22]
+values_dict = {'a': 20, 'b': 'Lol', 'c': 2.3}
+
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = [5.5, False]
+print_params(*values_list_2, 42)
+
